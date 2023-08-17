f97847cbaf213eaa996ab798cd6fc1c1fb7e5e1a
CBBell99 <chrisbeckertonbell@gmail.com>
2023-08-16 21:01:52 -0700

    privatize floormap

--- src/app/floormap/page.tsx
+++ src/app/floormap/page.tsx
@@ -1,41 +1,52 @@
-"use client";
+'use client';
 import { useState, useEffect } from 'react';
 import { useAppSelector } from '../context/store';
 import axios from 'axios';
-import { type } from 'os';
+import { redirect } from 'next/navigation';
 
 type Table = {
   id: number;
   available: boolean;
-}
+};
 
 function FloorMap() {
+  const user = useAppSelector(state => state.authReducer.user);
   const [tables, setTables] = useState([]);
 
-  const user = useAppSelector((state) => state.authReducer.user);
-  
+  if (!user.isAuth) {
+    redirect('/');
+  }
+
+  // console.log(user);
+
+ 
+
   useEffect(() => {
     const getTables = async () => {
-      const response = await axios.get('http://localhost:5005/api/tables')
-      const data = response.data
+      const response = await axios.get('http://localhost:5005/api/tables');
+      const data = response.data;
       console.log(data);
-      setTables(data)
-    }
-    getTables()
-}, []);
+      setTables(data);
+    };
+    getTables();
+  }, []);
 
   console.log(tables);
-  
-  
+
   return (
     <div>
       <nav>Welcome {user?.firstName}</nav>
-      <div>
-        {tables.map((table: Table, index) => (
-          <div className='bg-white border border-black text-black w-15 h-15 flex justify-center content-center hover:cursor-pointer' key={index}>{table.id}</div>
-        ))}
+      <div className='flex'>
+        {tables &&
+          tables.map((table: Table, index) => (
+            <div
+              className=' bg-white border border-black text-black w-[5rem] flex justify-center content-center hover:cursor-pointer'
+              key={index}
+            >
+              {table.id}
+            </div>
+          ))}
       </div>
-
     </div>
   );
 }
